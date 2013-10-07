501ac6d4336f37818f145dc272e5091abb270ebb
Spencer Schack <scs1818@gmail.com>
2013-10-06 22:20:00 -0700

    changing spacing on demo

--- public/javascripts/game/board.js
+++ public/javascripts/game/board.js
@@ -15,13 +15,12 @@ var Board = Class({
 		this._board = new Array(10);
 		for(var y = 0; y < 10; y++) {
 			var row    = this._board[y] = new Array(10)
-			  , yCoord = y * 30;
-			if(y.even) yCoord += 15;
+			  , yCoord = y * 16;
 			for(var x = 0; x < 10; x++) {
-				var xCoord = x * 30
-				  , tile   = new Tile(new Vector(xCoord, yCoord))
-				row[x] = tile;
-				tile.draw(this.context);
+				var xCoord = x * 16;
+				if(y.even) xCoord += 8;
+				row[x] = new Tile(new Vector(xCoord, yCoord));
+				row[x].draw(this.context);
 			}
 		}
 	}
